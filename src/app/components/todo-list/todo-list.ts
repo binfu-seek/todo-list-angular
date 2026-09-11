d4b602b9fd623426340da5b05e2b1ec475719bd6
binfu-seek <binfu.seek@gmail.com>
2026-09-11 16:45:14 +1000

    Add reactive form and successfully added todo to server

--- src/app/components/todo-list/todo-list.ts
+++ src/app/components/todo-list/todo-list.ts
@@ -1,16 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoService } from '../../services/todo.servise';
+import { ReactiveFormsModule, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { CreateTodoRequest } from '../../models/todo.models';
 
 @Component({
-  imports: [],
+  imports: [ReactiveFormsModule],
   selector: 'app-todo-list',
   styleUrl: './todo-list.scss',
   templateUrl: './todo-list.html',
 })
 export class TodoListComponent implements OnInit {
-  constructor(private readonly todoService: TodoService) {}
+  todoForm!: FormGroup<{
+    title: FormControl<string>;
+    description: FormControl<string>;
+    notes: FormControl<string>;
+  }>;
+
+  constructor(
+    private readonly todoService: TodoService,
+    private readonly fb: FormBuilder
+  ) {
+    this.todoForm = this.fb.nonNullable.group({
+      title: ['', [Validators.required, Validators.maxLength(100)]],
+      description: [''],
+      notes: ['']
+    });
+  }
 
   ngOnInit() {
     this.todoService.loadTodos().subscribe();
   }
+
+  onSubmit() {
+    if (this.todoForm.valid) {
+      const newTodo = this.todoForm.value as CreateTodoRequest;
+      this.todoService.addTodo(newTodo).subscribe(() => {
+        this.todoForm.reset();
+      });
+    }
+  } 
+
 }
