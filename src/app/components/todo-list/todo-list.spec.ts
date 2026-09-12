c384cc229fedbb4bde6b6d66d9f0bed5717d9e2a
binfu-seek <binfu.seek@gmail.com>
2026-09-12 13:43:19 +1000

    Add mocking to todo-list.spec. Basic test case pass

--- src/app/components/todo-list/todo-list.spec.ts
+++ src/app/components/todo-list/todo-list.spec.ts
@@ -1,16 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { TodoList } from './todo-list';
+import { TodoListComponent } from './todo-list';
+import { signal } from '@angular/core';
+import { of } from 'rxjs';
+import { vi } from 'vitest';
+import { TodoService } from '../../services/todo.servise';
 
-describe('TodoList', () => {
-  let component: TodoList;
-  let fixture: ComponentFixture<TodoList>;
+describe('TodoListComponent', () => {
+  let component: TodoListComponent;
+  let fixture: ComponentFixture<TodoListComponent>;
+
+  const todoServiceMock = {
+    todos$: signal([]),
+    loadTodos: vi.fn(() => of([])),
+    addTodo: vi.fn(() => of({})),
+    deleteTodo: vi.fn(() => of({})),
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TodoList],
+      imports: [TodoListComponent],
+      providers: [
+        { provide: TodoService, useValue: todoServiceMock }
+      ]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(TodoList);
+    fixture = TestBed.createComponent(TodoListComponent);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
